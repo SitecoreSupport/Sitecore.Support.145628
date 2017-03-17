298333b551f20a5bf840cf719cb67ee9b302093f
DK\DTAT <dtat@sitecore.net>
2017-03-17 09:13:42 +0200

    Commit patch

---
+++ src/Sitecore.Support.145628/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.145628")]
+[assembly: AssemblyProduct("Sitecore.Support.145628")]
+[assembly: ComVisible(false)]
